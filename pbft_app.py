1a2faadd77c8143ceb1a626665152b5847ada440
sinamrt <45807107+sinamrt@users.noreply.github.com>
2023-12-18 15:13:03 +1100

    Add files via upload
    Running application module, aiming for integrations

--- pbft_app.py
+++ pbft_app.py
@@ -11,53 +11,49 @@ from pbft_p2p_server import P2pServer  # Assuming you have a P2pServer class def
   # Assuming you have NUMBER_OF_NODES defined in config module
 from pbft_config import config
 
-new_config = config(5, 3, 2)
-
-
-# Update the parameters using setters
-new_config.TRANSACTION_THRESHOLD = 10
-new_config.NUMBER_OF_NODES = 5
-new_config.MIN_APPROVALS = 2 * (new_config.NUMBER_OF_NODES / 3) + 1
-
-# # Access the parameters using getters
-# print("TRANSACTION_THRESHOLD:", new_config.TRANSACTION_THRESHOLD)
-# print("NUMBER_OF_NODES:", new_config.NUMBER_OF_NODES)
-# print("MIN_APPROVALS:", new_config.MIN_APPROVALS)
-# #
 app = Flask(__name__)
-secret = "your_secret_here"
-# secret_bytes = secret.encode('utf-8')
-# print(secret_bytes)
-# secret_bytes.decode()
 
-new_wallet = Wallet(secret)  # Replace with your actual secret
-transaction_pool = TransactionPool()
-validators = Validators(new_config.NUMBER_OF_NODES)
-blockchain = Blockchain(validators, new_config.NUMBER_OF_NODES)
-block_pool = BlockPool()
-prepare_pool = PreparePool()
-commit_pool = CommitPool()
-message_pool = MessagePool()
-p2p_server = P2pServer(
-    blockchain, transaction_pool, new_wallet, block_pool,
-    prepare_pool, commit_pool, message_pool, validators
-)
+@app.route("/transactions", methods=["GET"])
+def get_transactions():
+    return jsonify(transaction_pool.transactions), 200
 
-# @app.route("/transactions", methods=["GET"])
-# def get_transactions():
-#     return jsonify(transaction_pool.transactions), 200
+@app.route("/blocks", methods=["GET"])
+def get_blocks():
+    return jsonify(blockchain.chain), 200
+
+@app.route("/transact", methods=["POST"])
+def transact():
+    data = request.json.get("data")
+    transaction = new_wallet.create_transaction(data)
+    p2p_server.broadcast_transaction(transaction)
+    return redirect("/transactions"), 303
 #
-# @app.route("/blocks", methods=["GET"])
-# def get_blocks():
-#     return jsonify(blockchain.chain), 200
-#
-# @app.route("/transact", methods=["POST"])
-# def transact():
-#     data = request.json.get("data")
-#     transaction = wallet.create_transaction(data)
-#     p2p_server.broadcast_transaction(transaction)
-#     return redirect("/transactions"), 303
-#
-# if __name__ == "__main__":
-#     app.run(port=3001)
-#     p2p_server.listen()
+if __name__ == "__main__":
+    NUMBER_OF_NODES = int(input("enter number of nodes "))
+    secret = input("Please enter your secret ")
+    port_number = input("enter the port number ")
+    TRANSACTION_THRESHOLD = 10
+    new_config = config(NUMBER_OF_NODES, TRANSACTION_THRESHOLD, 2)
+
+    # Update the parameters using setters
+    #new_config.NUMBER_OF_NODES = 5
+    new_config.MIN_APPROVALS = 2 * (NUMBER_OF_NODES / 3) + 1
+
+
+    #secret = "your_secret_here"
+
+    new_wallet = Wallet(secret)  # Replace with your actual secret
+    transaction_pool = TransactionPool()
+    validators = Validators(NUMBER_OF_NODES)
+    blockchain = Blockchain(validators, NUMBER_OF_NODES)
+    block_pool = BlockPool()
+    prepare_pool = PreparePool()
+    commit_pool = CommitPool()
+    message_pool = MessagePool()
+    p2p_server = P2pServer(
+        blockchain, transaction_pool, new_wallet, block_pool,
+        prepare_pool, commit_pool, message_pool, validators
+    )
+
+    app.run(port=port_number)
+    p2p_server.listen()
