a64188a3660199c116244752415bfe818ed79b7c
NoorullahMuhammadKhan <noorullahkhan@outlook.com>
2023-12-21 14:15:49 +1100

    Making Progress to the csv file which has all the data output, but just a slight error in my terminal output

--- FurtherPrototypeWork.py
+++ FurtherPrototypeWork.py
@@ -14,18 +14,24 @@ from datetime import datetime
 NUMBER_OF_NODES = 10
 FAULTY_PROPORTION = 1/3
 NUMBER_OF_FAULTY_NODES = math.floor(NUMBER_OF_NODES * FAULTY_PROPORTION)
-CHUNK_SIZE = 500 * 1  # Adjusted chunk size
+CHUNK_SIZE = 500  # Adjusted chunk size
 BUFFER_CHECK_FREQUENCY = 20  # Seconds
 DATA_BUFFER = ""  # Initialize data buffer
 exit_flag = False  # Flag for graceful exit
 
-# Define the number of segments and chunks
-number_of_segments = 5  # Adjust this to your desired number
-number_of_chunks = 3  # Adjust this to your desired number
+NUMBER_OF_SEGMENTS = 5  # Define the number of segments per chunk
+NUMBER_OF_CHUNKS = 3    # Define the number of chunks to process
 
 # Generate RSA keys (private and public)
 private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 public_key = private_key.public_key()
+
+
+# CSV File Initialization
+csv_filename = 'data.csv'
+with open(csv_filename, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Timestamp", "Chunk Data", "Chunk Size", "Digital Signature", "Verification Outcome", "True Vote Percentage"])
 
 # Node Class
 class Node:
@@ -73,10 +79,7 @@ def segment_data(data, private_key):
     if len(data) < 250:
         return []
 
-    segments = [data[:100], data[-100:]]  # First and last 100 characters
-    for _ in range(5):
-        start_index = random.randint(100, len(data) - 150)
-        segments.append(data[start_index:start_index + 50])
+    segments = [data[i:i + CHUNK_SIZE // NUMBER_OF_SEGMENTS] for i in range(0, len(data), CHUNK_SIZE // NUMBER_OF_SEGMENTS)]
 
     segment_info = []
     for segment in segments:
@@ -94,69 +97,70 @@ def segment_data(data, private_key):
 
     return segment_info
 
+# Function to save chunk data to CSV
+def save_to_csv(timestamp, chunk_data, chunk_size, digital_signature, outcome, true_vote_percentage):
+    with open(csv_filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([timestamp, chunk_data, chunk_size, digital_signature, outcome, true_vote_percentage])
+
 # Function to process and clear the buffer
 def process_buffer(dht, chunk_number):
     global DATA_BUFFER
-    print(f"Checking data for chunk {chunk_number}...")  # Debugging print
     if len(DATA_BUFFER) >= CHUNK_SIZE:
-        print(f"Processing chunk {chunk_number}...")  # Debugging print
-        segments_info = segment_data(DATA_BUFFER[:CHUNK_SIZE], private_key)
+        chunk_data = DATA_BUFFER[:CHUNK_SIZE]
         DATA_BUFFER = DATA_BUFFER[CHUNK_SIZE:]
 
-        chunk_consensus = True  # Initialize consensus for the chunk
+        print(f"Processing Chunk {chunk_number}...")
+        segments_info = segment_data(chunk_data, private_key)
+        node_votes = {node_id: [] for node_id in range(NUMBER_OF_NODES)}
+        chunk_consensus = True
         total_true_votes = 0
 
-        try:
-            with open('websocket_data.csv', mode='a', newline='') as file:
-                writer = csv.writer(file)
-                for segment, segment_hash, signature, timestamp in segments_info:
-                    segment_votes = dht.process_segment(segment, segment_hash, signature, timestamp)
-                    for node_id in range(NUMBER_OF_NODES):
-                        node_votes[node_id].append(segment_votes[node_id])
-                        if segment_votes[node_id]:
-                            total_true_votes += 1
-                    chunk_consensus &= (sum(segment_votes.values()) >= math.ceil(0.7 * NUMBER_OF_NODES))
+        for segment_index, (segment, segment_hash, signature, timestamp) in enumerate(segments_info):
+            segment_votes = dht.process_segment(segment, segment_hash, signature, timestamp)
+            print(f"Segment {segment_index + 1} Votes:", segment_votes)
+            for node_id, vote in segment_votes.items():
+                node_votes[node_id].append(vote)
+                if vote:
+                    total_true_votes += 1
+            chunk_consensus &= (sum(segment_votes.values()) >= math.ceil(0.7 * NUMBER_OF_NODES))
 
-                    # Writing to CSV
-                    writer.writerow([chunk_number, segment, segment_hash, signature.hex(), timestamp, 'Verified Successfully' if chunk_consensus else 'Verified Unsuccessfully'])
-            print(f"Chunk {chunk_number} data written to CSV.")  # Debugging print
-        except Exception as e:
-            print(f"Error writing to CSV file: {e}")  # Exception print
-    else:
-        print(f"Not enough data for chunk {chunk_number}.")  # Debugging print
+        for node_id, votes in node_votes.items():
+            print(f"Node {node_id} Votes: {votes}")
+
+        total_votes = len(segments_info) * NUMBER_OF_NODES
+        true_vote_percentage = (total_true_votes / total_votes) * 100
+        print(f"Chunk {chunk_number} Verified Successfully" if chunk_consensus else "Verified Unsuccessfully", f"({true_vote_percentage:.2f}% true)")
+
+        # Compute digital signature for the entire chunk
+        chunk_signature = private_key.sign(
+            chunk_data.encode(),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        chunk_signature_readable = chunk_signature.hex()
+
+        # Save chunk data to CSV
+        save_to_csv(datetime.now().timestamp(), chunk_data, len(chunk_data), chunk_signature_readable, chunk_verification_outcome, true_vote_percentage)
 
 # Function to check buffer size periodically
-def process_buffer(dht, chunk_number):
-    global DATA_BUFFER
-    print(f"Checking data for chunk {chunk_number}...")  # Debugging print
-    if len(DATA_BUFFER) >= CHUNK_SIZE:
-        print(f"Processing chunk {chunk_number}...")  # Debugging print
-        segments_info = segment_data(DATA_BUFFER[:CHUNK_SIZE], private_key)
-        DATA_BUFFER = DATA_BUFFER[CHUNK_SIZE:]
-
-        node_votes = {node_id: [] for node_id in range(NUMBER_OF_NODES)}  # Initialize node_votes
-        chunk_consensus = True  # Initialize consensus for the chunk
-        total_true_votes = 0
-
-        try:
-            with open('websocket_data.csv', mode='a', newline='') as file:
-                writer = csv.writer(file)
-                for segment, segment_hash, signature, timestamp in segments_info:
-                    segment_votes = dht.process_segment(segment, segment_hash, signature, timestamp)
-                    for node_id in range(NUMBER_OF_NODES):
-                        node_votes[node_id].append(segment_votes[node_id])
-                        if segment_votes[node_id]:
-                            total_true_votes += 1
-                    chunk_consensus &= (sum(segment_votes.values()) >= math.ceil(0.7 * NUMBER_OF_NODES))
-
-                    # Writing to CSV
-                    writer.writerow([chunk_number, segment, segment_hash, signature.hex(), timestamp, 'Verified Successfully' if chunk_consensus else 'Verified Unsuccessfully'])
-            print(f"Chunk {chunk_number} data written to CSV.")  # Debugging print
-        except Exception as e:
-            print(f"Error writing to CSV file: {e}")  # Exception print
-    else:
-        print(f"Not enough data for chunk {chunk_number}.")  # Debugging print
-
+def check_buffer_size(dht):
+    global exit_flag
+    chunk_number = 1
+    while not exit_flag and chunk_number <= NUMBER_OF_CHUNKS:
+        if len(DATA_BUFFER) >= CHUNK_SIZE:
+            process_buffer(dht, chunk_number)
+            if chunk_number == NUMBER_OF_CHUNKS:
+                exit_flag = True  # Set exit flag to ensure main program exits
+                break
+            chunk_number += 1
+        else:
+            print(f"Waiting for enough data for Chunk {chunk_number}...")
+        time.sleep(BUFFER_CHECK_FREQUENCY)
+    print("Exiting buffer check thread.")
 
 # Binance WebSocket message processing function
 def process_binance_message(ws_message):
@@ -180,11 +184,10 @@ def on_close(ws, close_status_code, close_msg):
 def on_open(ws):
     print("WebSocket connection opened to Binance")
 
-# Set up Binance WebSocket connection
+# Set up Binance WebSocket connection and run client
 binance_ws_url = "wss://stream.binance.com:9443/ws/btcusdt@trade"
 ws = websocket.WebSocketApp(binance_ws_url, on_message=on_message, on_error=on_error, on_close=on_close)
 
-# Run the WebSocket client and buffer checker in separate threads
 dht = DHT(public_key)
 ws_thread = threading.Thread(target=ws.run_forever)
 ws_thread.start()
@@ -196,7 +199,7 @@ buffer_thread.start()
 try:
     while True:
         if exit_flag:
-            break  # Break the loop if exit_flag is True
+            break
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("Exiting program...")
